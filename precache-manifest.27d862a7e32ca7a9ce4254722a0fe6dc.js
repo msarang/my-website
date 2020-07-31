d34d7fad03ebf27ad08f9806c0461aa4068e0576
msarang <mehak.sarang@gmail.com>
2020-07-30 22:20:20 -0500

    Updates

---
+++ precache-manifest.27d862a7e32ca7a9ce4254722a0fe6dc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0ee9b8445331fb9b8f88c8a377f5f0fc",
+    "url": "/my-website/index.html"
+  },
+  {
+    "revision": "1f1512047e05ba1e53a1",
+    "url": "/my-website/static/css/main.6b8f59a2.chunk.css"
+  },
+  {
+    "revision": "a170336d57d2ba7a5d2c",
+    "url": "/my-website/static/js/2.696b64ae.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/my-website/static/js/2.696b64ae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1f1512047e05ba1e53a1",
+    "url": "/my-website/static/js/main.b059211d.chunk.js"
+  },
+  {
+    "revision": "dc2e3b3511453d8cdedf",
+    "url": "/my-website/static/js/runtime-main.dac0fb3d.js"
+  },
+  {
+    "revision": "16bd41972719b9b0c5f2ac984d3c6015",
+    "url": "/my-website/static/media/star-background.16bd4197.jpg"
+  }
+]);
