f76ccf68e200d8fc141e4c6af98c87cf42cd1a60
msarang <mehak.sarang@gmail.com>
2020-07-30 23:10:57 -0500

    hash

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ class App extends Component {
   render() {   
   return (
     <Router basename="/">
-         <Route exact path="/" component={Home} />
+         <Route exact path="/"> <Home /> </Route>
    </Router>
   );
 }
