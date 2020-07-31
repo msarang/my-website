6e1b8fe6dd61d600915f63aa3db20fd66983e5c6
msarang <mehak.sarang@gmail.com>
2020-07-30 23:26:04 -0500

    Update precache-manifest.6d9b1d1fe0e566e1d23860de2bed4bc5.js
    build fix

--- precache-manifest.6d9b1d1fe0e566e1d23860de2bed4bc5.js
+++ precache-manifest.6d9b1d1fe0e566e1d23860de2bed4bc5.js
@@ -1,30 +1,30 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
     "revision": "b278ec5ac16b63245b5f871d570657c1",
-    "url": "/mehaksarang.com/index.html"
+    "url": "/index.html"
   },
   {
     "revision": "59588a5a59bf1b6d84a0",
-    "url": "/mehaksarang.com/static/css/main.d3bf327b.chunk.css"
+    "url": "/static/css/main.d3bf327b.chunk.css"
   },
   {
     "revision": "a170336d57d2ba7a5d2c",
-    "url": "/mehaksarang.com/static/js/2.696b64ae.chunk.js"
+    "url": "/static/js/2.696b64ae.chunk.js"
   },
   {
     "revision": "3453b8997016469371284a28c0e873e2",
-    "url": "/mehaksarang.com/static/js/2.696b64ae.chunk.js.LICENSE.txt"
+    "url": "/static/js/2.696b64ae.chunk.js.LICENSE.txt"
   },
   {
     "revision": "59588a5a59bf1b6d84a0",
-    "url": "/mehaksarang.com/static/js/main.c0aab741.chunk.js"
+    "url": "/static/js/main.c0aab741.chunk.js"
   },
   {
     "revision": "ca217d319529944899ef",
-    "url": "/mehaksarang.com/static/js/runtime-main.1e334ddc.js"
+    "url": "/static/js/runtime-main.1e334ddc.js"
   },
   {
     "revision": "16bd41972719b9b0c5f2ac984d3c6015",
-    "url": "/mehaksarang.com/static/media/star-background.16bd4197.jpg"
+    "url": "/static/media/star-background.16bd4197.jpg"
   }
 ]);
