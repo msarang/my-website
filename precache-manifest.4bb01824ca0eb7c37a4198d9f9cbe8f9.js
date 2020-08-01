1155ea637f832266a6632fa6c58fa3d7db26dbfc
msarang <mehak.sarang@gmail.com>
2020-08-01 10:10:03 -0500

    Updates

---
+++ precache-manifest.4bb01824ca0eb7c37a4198d9f9cbe8f9.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "16f6f94426c9ca5eb0701784d951de31",
+    "url": "./index.html"
+  },
+  {
+    "revision": "4910838eea0327ec96dc",
+    "url": "./static/css/main.86237af9.chunk.css"
+  },
+  {
+    "revision": "bd5c35594d9d4b8b1bea",
+    "url": "./static/js/2.f8c2c59c.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "./static/js/2.f8c2c59c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4910838eea0327ec96dc",
+    "url": "./static/js/main.e2f1f69f.chunk.js"
+  },
+  {
+    "revision": "913670389d24466c1977",
+    "url": "./static/js/runtime-main.8893b54b.js"
+  },
+  {
+    "revision": "b57b1ae68c541c5b7152b657d1c8e26a",
+    "url": "./static/media/Headshot.b57b1ae6.png"
+  },
+  {
+    "revision": "16bd41972719b9b0c5f2ac984d3c6015",
+    "url": "./static/media/star-background.16bd4197.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/mehaksarang.com/precache-manifest.3e88ecec6860399e6b33229423440508.js"
+  "./precache-manifest.4bb01824ca0eb7c37a4198d9f9cbe8f9.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/mehaksarang.com/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
